c015c61b83b9cbea6d8bd3c59b9ef4f2fd3fff0d
jasperayawan <ejayawan22@gmail.com>
2023-08-15 18:42:20 +0800

    delete API update

--- tlex-backend/routes/post.js
+++ tlex-backend/routes/post.js
@@ -31,4 +31,22 @@ router.put('/:id', async (req, res) => {
     }
 })
 
+
+// delete post
+
+
+router.delete('/:id', async (req, res) => {
+    try{
+        const post = await Post.findById(req.params.id);
+        if(post.userId === req.body.userId){
+            await post.deleteOne();
+            res.status(200).json("the post has been deleted!")
+        } else {
+            res.status(403).json("you can delete only your post!")
+        }
+    }
+    catch(error){
+        res.status(500).json(error)
+    }
+})
 module.exports = router

aacf9213dd9546606459d5eff1fdeeb4f2330371
joshalbert2433 <joshua.albert.villanueva@gmail.com>
2023-08-20 12:01:43 +0800

    fix nested routing

--- client/src/pages/Registration/Registration.jsx
+++ client/src/pages/Registration/Registration.jsx
@@ -2,147 +2,158 @@ import { church } from "../../helper/dummy_image/dummyImage";
 import { Link, useNavigate } from "react-router-dom";
 import Axios from "axios";
 import { useRef, useState } from "react";
-import { useToast } from '@chakra-ui/react'
+import { useToast } from "@chakra-ui/react";
 
 import { REGISTER_API } from "../../api/api_list";
 
 export default function Registration() {
-  const username = useRef();
-  const email = useRef();
-  const password = useRef();
-  const confirmPassword = useRef();
-  const Navigate = useNavigate();
-  const toast = useToast()
+	const username = useRef();
+	const email = useRef();
+	const password = useRef();
+	const confirmPassword = useRef();
+	const Navigate = useNavigate();
+	const toast = useToast();
 
-  const handleRegistration = async (e) => {
-    e.preventDefault();
+	const handleRegistration = async (e) => {
+		e.preventDefault();
 
-    if(confirmPassword.current.value !== password.current.value){
-      password.current.setCustomValidity("Password don't match!")
-    } else {
+		if (confirmPassword.current.value !== password.current.value) {
+			password.current.setCustomValidity("Password don't match!");
+		} else {
+			const user = {
+				username: username.current.value,
+				email: email.current.value,
+				password: password.current.value,
+			};
 
-      const user = {
-        username: username.current.value,
-        email: email.current.value,
-        password: password.current.value
-      }
+			try {
+				const response = await Axios.post(REGISTER_API, user);
 
-      try {
-        const response = await Axios.post(
-          REGISTER_API, user);
-  
-        if (response.status === 200) {
-          toast({
-            title: 'Account successfully created!',
-            description: "We've created your account for you.",
-            status: 'success',
-            duration: 3000,
-            isClosable: true,
-            position: 'top-right'
-          })
-          Navigate("/");
-        }
-      } catch (error) {
-        toast({
-          title: "Failed creating an account!",
-          description: "Please check your email and password.",
-          status: "error",
-          duration: 3000,
-          isClosable: true,
-          position: 'top-right'
-        });
-      }
-    }
+				if (response.status === 200) {
+					toast({
+						title: "Account successfully created!",
+						description: "We've created your account for you.",
+						status: "success",
+						duration: 3000,
+						isClosable: true,
+						position: "top-right",
+					});
+					Navigate("/");
+				}
+			} catch (error) {
+				toast({
+					title: "Failed creating an account!",
+					description: "Please check your email and password.",
+					status: "error",
+					duration: 3000,
+					isClosable: true,
+					position: "top-right",
+				});
+			}
+		}
+	};
+	return (
+		<div>
+			<div className=" text-white bg-white flex overflow-auto">
+				<div className="mx-auto h-screen flex justify-center items-center p-20">
+					<div className="flex flex-col w-full mx-auto max-w-lg px-4 py-4 rounded-md">
+						<div className="flex flex-col gap-y-2 mb-4">
+							<h1 className="text-5xl font-bold text-black text-center">
+								Create an Account!
+							</h1>
+							<span className="text-md tracking-wider text-center text-black">
+								Welcome back to Christ community!
+							</span>
+						</div>
 
-    
-  };
-  return (
-    <div>
-      <div className=" text-white bg-white flex overflow-auto">
-        <div className="mx-auto h-screen flex justify-center items-center p-20">
-          <div className="flex flex-col w-full mx-auto max-w-lg px-4 py-4 rounded-md">
-            <div className="flex flex-col gap-y-2 mb-4">
-              <h1 className="text-5xl font-bold text-black text-center">
-                Create an Account!
-              </h1>
-              <span className="text-md tracking-wider text-center text-black">
-                Welcome back to Christ community!
-              </span>
-            </div>
-
-            <form onSubmit={handleRegistration}>
-              <div className="flex flex-col gap-y-2">
-                <div className="flex flex-col gap-x-10 justify-start">
-                  <label htmlFor="username" className="font-bold text-zinc-600">
-                    username
-                  </label>
-                  <input
-                    ref={username}
-                    type="text"
-                    placeholder="username"
-                    required
-                    className="ring-1 ring-zinc-800 text-gray-500 px-4 py-2 pr-20 rounded-md"
-                  />
-                </div>
-                <div className="flex flex-col gap-x-10 justify-start">
-                  <label htmlFor="name" className="font-bold text-zinc-600">
-                    Email
-                  </label>
-                  <input
-                    ref={email}
-                    type="text"
-                    placeholder="Email"
-                    required
-                    className="ring-1 ring-zinc-800 text-gray-500 px-4 py-2 pr-20 rounded-md"
-                  />
-                </div>
-                <div className="flex flex-col gap-x-10 justify-start">
-                  <label htmlFor="password" className="font-bold text-zinc-600">
-                    Password
-                  </label>
-                  <input
-                    ref={password}
-                    minLength="6"
-                    type="password"
-                    placeholder="Enter Passowrd"
-                    required
-                    className="ring-1 ring-zinc-800 text-gray-500 px-4 py-2 pr-20 rounded-md"
-                  />
-                </div>
-                <div className="flex flex-col gap-x-10 justify-start">
-                  <label htmlFor="password" className="font-bold text-zinc-600">
-                    Confirm Password
-                  </label>
-                  <input
-                    ref={confirmPassword}
-                    minLength="6"
-                    type="password"
-                    placeholder="Confirm Password"
-                    required
-                    className="ring-1 ring-zinc-800 text-gray-500 px-4 py-2 pr-20 rounded-md"
-                  />
-                </div>
-                <div className="flex justify-end">
-                <span className="text-black">
-                  Already have an account?{" "}
-                  <Link to="/" className="text-[#59A52C] font-bold">
-                    login!
-                  </Link>
-                </span>
-              </div>
-                <div className="flex justify-center items-center mt-4">
-                  <button
-                    type="submit"
-                    className="px-10 py-2 rounded-md w-full bg-black"
-                  >
-                    Register
-                  </button>
-                </div>
-              </div>
-            </form>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+						<form onSubmit={handleRegistration}>
+							<div className="flex flex-col gap-y-2">
+								<div className="flex flex-col gap-x-10 justify-start">
+									<label
+										htmlFor="username"
+										className="font-bold text-zinc-600"
+									>
+										username
+									</label>
+									<input
+										ref={username}
+										type="text"
+										placeholder="username"
+										required
+										className="ring-1 ring-zinc-800 text-gray-500 px-4 py-2 pr-20 rounded-md"
+									/>
+								</div>
+								<div className="flex flex-col gap-x-10 justify-start">
+									<label
+										htmlFor="name"
+										className="font-bold text-zinc-600"
+									>
+										Email
+									</label>
+									<input
+										ref={email}
+										type="text"
+										placeholder="Email"
+										required
+										className="ring-1 ring-zinc-800 text-gray-500 px-4 py-2 pr-20 rounded-md"
+									/>
+								</div>
+								<div className="flex flex-col gap-x-10 justify-start">
+									<label
+										htmlFor="password"
+										className="font-bold text-zinc-600"
+									>
+										Password
+									</label>
+									<input
+										ref={password}
+										minLength="6"
+										type="password"
+										placeholder="Enter Passowrd"
+										required
+										className="ring-1 ring-zinc-800 text-gray-500 px-4 py-2 pr-20 rounded-md"
+									/>
+								</div>
+								<div className="flex flex-col gap-x-10 justify-start">
+									<label
+										htmlFor="password"
+										className="font-bold text-zinc-600"
+									>
+										Confirm Password
+									</label>
+									<input
+										ref={confirmPassword}
+										minLength="6"
+										type="password"
+										placeholder="Confirm Password"
+										required
+										className="ring-1 ring-zinc-800 text-gray-500 px-4 py-2 pr-20 rounded-md"
+									/>
+								</div>
+								<div className="flex justify-end">
+									<span className="text-black">
+										Already have an account?{" "}
+										<Link
+											to="/"
+											className="text-[#59A52C] font-bold"
+										>
+											login!
+										</Link>
+									</span>
+								</div>
+								<div className="flex justify-center items-center mt-4">
+									<button
+										type="submit"
+										className="px-10 py-2 rounded-md w-full bg-black"
+									>
+										Register
+									</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 }
--- client/src/routes/routes.jsx
+++ client/src/routes/routes.jsx
@@ -1,33 +1,34 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import App from '../App'
-import { SkeletonTheme } from 'react-loading-skeleton'
-import Registration from '../pages/Registration/Registration'
-import Login from '../pages/Login/Login'
-import Forget from '../pages/Forget/forget'
-import Hero from '../components/hero/hero'
-import Profile from '../pages/Profile/Profile'
-import { useSelector } from 'react-redux'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "../App";
+import { SkeletonTheme } from "react-loading-skeleton";
+import Registration from "../pages/Registration/Registration";
+import Login from "../pages/Login/Login";
+import Forget from "../pages/Forget/forget";
+import Hero from "../components/hero/hero";
+import Profile from "../pages/Profile/Profile";
+import { useSelector } from "react-redux";
 
-export default function Router(){
-const user = useSelector((data) => data.user.username)
+export default function Router() {
+	const user = useSelector((data) => data.user.username);
 
-    return(
-        <SkeletonTheme baseColor='#313131' highlightColor='#525252'>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/index' element={<App/>}>
-                        {user 
-                        ? <Route exact path='/index' element={<Hero/>}/> 
-                        : <Route path='/register' element={<Registration/>}/>
-                        }
-                    </Route>
-                    
-                    <Route path='/profile' element={<Profile/>}/>
-                    <Route path='/register' element={<Registration/>}/>
-                    <Route path='/' element={<Login/>}/>
-                    <Route path='/forget' element={<Forget/>}/>
-                </Routes>
-            </BrowserRouter>
-        </SkeletonTheme>
-    )
+	return (
+		<SkeletonTheme baseColor="#313131" highlightColor="#525252">
+			<BrowserRouter>
+				<Routes>
+					<Route path="/index" element={<App />}>
+						{user ? (
+							<Route exact path="/index" element={<Hero />} />
+						) : (
+							<Route path="login" element={<Registration />} />
+						)}
+					</Route>
+
+					<Route path="/profile" element={<Profile />} />
+					<Route path="/register" element={<Registration />} />
+					<Route path="/" element={<Login />} />
+					<Route path="/forget" element={<Forget />} />
+				</Routes>
+			</BrowserRouter>
+		</SkeletonTheme>
+	);
 }
